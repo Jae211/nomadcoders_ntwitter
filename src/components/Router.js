49dfb4b9950261dd8dc6843d58ee45a73d1c8662
Jae211 <sally1926@naver.com>
2022-09-08 18:07:19 +0900

    Authentication

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,29 +1,37 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Redirect } from "react-router";
 
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import Profile from "routes/Profile";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {/* <Navigation/> if isLoggedIn is true */}
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
-              <Home/>
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </>
         ) : (
-          <Route exact path="/">
-            <Auth/>
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
         )}
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
 export default AppRouter;
